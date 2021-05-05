9a83c6665e69cace6bbe5281bc30a8a1095384a9
Ali Ghaleb <73604031+alighaleb1@users.noreply.github.com>
2021-05-05 00:28:55 -0400

    Priority Queue
    priority Queue (some values have the right to be served more than others).

---
+++ DataStructuresandAlogrithms/PQueue/PriorityQ.java
@@ -0,0 +1,56 @@
+package PQueue;
+
+public class PriorityQ 
+{
+	// array in sorted order, from max at 0 to min at size-1 private
+	
+	int maxSize;
+	private long[] queArray;
+	private int nItems;
+	
+	public PriorityQ(int s) 
+	{
+		maxSize = s;
+		queArray = new long[maxSize];
+		nItems = 0;
+	}
+		// constructor
+	
+	public void insert(long item) 
+	{
+		int j;
+		if(nItems==0) 
+			queArray[nItems++] = item;
+		else
+		{
+			for(j=nItems-1; j>=0; j--) 
+			{
+				if( item > queArray[j] )
+					queArray[j+1] = queArray[j]; // shift upward
+				else
+					break;
+			} // end for 
+			queArray[j+1] = item; 
+			nItems++;
+		} 
+	}
+	
+	public long remove() 
+	{
+		return queArray[--nItems];
+	}
+	
+	public long peekMin() 
+	{
+		return queArray[nItems-1]; 
+	} 
+	
+	public boolean isEmpty() 
+	{
+		return (nItems==0);
+	}
+	
+	public boolean isFull() 
+	{ 
+		return (nItems == maxSize); } 
+	} 
